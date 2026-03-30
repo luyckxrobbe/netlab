745dacd3dccc9ddced93f8d119c8ea2e04b965d3
Robbe Luyckx <luyckxrobbe@gmail.com>
2026-03-30 10:29:01 +0200

    rfi

---
+++ rfi.php
@@ -0,0 +1,11 @@
+<?php
+$file = "/var/ww/html/hackable/flags/fi.php";
+
+if (file_exists($file)) {
+    echo "<pre>";
+    echo htmlspecialchars(file_get_contents($file));
+    echo "</pre>";
+} else {
+    echo "Bestand niet gevonden.";
+}
+?>
